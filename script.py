fb4b8d15edaeb626390537a90d47033d54415a10
Soufiane <hamida@enset-media.ac.ma>
2025-09-23 23:29:43 +0100

    randomForest Estimators 1000

--- script.py
+++ script.py
@@ -119,7 +119,7 @@ def train_model(X_train, y_train, plot_name='', class_weight=None):
     
     global clf_name
 
-    clf = RandomForestClassifier(n_estimators=500, max_depth=10, random_state=45, class_weight=class_weight)
+    clf = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=45, class_weight=class_weight)
     clf.fit(X_train, y_train)
     y_pred_train = clf.predict(X_train)
     y_pred_test = clf.predict(X_test_final)
